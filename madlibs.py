23778886a306f64c13f8baab75c5ea9e1d22c793
jmnieves12 <natureshadowlock723@gmail.com>
2024-01-29 16:21:35 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,22 @@
 # TODO Create your inputs, variables, and add your story here!
+noun = input("Enter a noun: ")
+adjective = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+
+print(f"{noun} was {adjective} while {verb}")
+
+# Add 10 more inputs, update your story along the way
+
+#input one
+noun2 = input("Enter another noun: ")
+#input two
+person1 = input ("Enter the name of your favorite person: ")
+#input 3
+noun3 = input("Enter a silly noun: ")
+#input four
+verb2 = input("Enter a dangerous verb: ")
+print(f"{noun2} continued down the road when {person1} saw a {noun3} and decided to {verb2}.")
+
+print("Good job! You're on your way to becoming a storyteller.")
+#input five
+noun4 = input("Enter a place.")
